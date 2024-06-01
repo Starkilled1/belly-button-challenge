06967d82b1d15c4d7aa3b08bfd2b9f93bb63bea9
Javier Alarcon <javieralarconbusiness@outlook.com>
2024-06-01 10:28:06 -0700

    Added test Subject functionality

--- static/js/app.js
+++ static/js/app.js
@@ -3,19 +3,24 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let metadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let filterdmetadata = metadata.filter(sampleobj => sampleobj.id == sample)[0];
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
+    for(let [key, value] of Object.entries(filterdmetadata)){
+      
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    }
+
 
   });
 }
@@ -56,28 +61,33 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-
+    let sampleNames = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select("#selDataset");
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
+    for(let i = 0; i<sampleNames.length; i++){
+      dropdown.append("option").text(sampleNames[i]).property("value", sampleNames[i]);
 
+    }
 
     // Get the first sample from the list
-
+    let firstSample = sampleNames[0];
 
     // Build charts and metadata panel with the first sample
-
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialize the dashboard
